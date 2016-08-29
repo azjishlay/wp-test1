c440b287bd6d62adfd0fda4c36c521e0aa1a42be
lijasminej <li.jasmine.j@gmail.com>
2016-08-28 23:21:46 -0400

    Update fonts

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,9 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="title" content="<?php bloginfo('title'); ?>">
+  <meta name="description" content="<?php bloginfo('description'); ?>">
   <?php wp_head(); ?>
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
 </head>
