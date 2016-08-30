10c1db8e364cb68932ae1e936e18c360a74c6b74
lijasminej <li.jasmine.j@gmail.com>
2016-08-29 23:43:32 -0400

    Footer styles

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,22 @@
 <footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+    <div class="container">
+        <div class="row">
+            <hr />
+            <div id="col-left" class="col-xs-12 col-sm-3">
+                <p class="caps">&copy; 2016 Lorem Ipsum</p>
+            </div>
+            <div id="col-center" class="col-xs-12 col-sm-6">
+                <p class="caps">80 Lorem St, Ipsum, NJ 000000</p>
+            </div>
+            <div id="col-right" class="col-xs-12 col-sm-3">
+                <p class="caps">555.555.5555</p>
+            </div>
+        </div>
+        <div class="row">
+            <p class="header-tag color2">Lorem ipsum dolor sit amet, eu est summo scripserit</p>
+        </div>
+        <div class="row">
+            <img src="<?= get_template_directory_uri(); ?>/dist/images/computer-new.png" height="300px" />
+        </div>
+    </div>
 </footer>
